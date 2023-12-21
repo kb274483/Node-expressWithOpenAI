5df3977511e5f85d8b8cb5c1b1651ff296f56f14
Roy <roy@gastom.com.tw>
2023-12-21 11:33:41 +0800

    adjust timeout length

--- index.js
+++ index.js
@@ -91,4 +91,7 @@ app.post("/node_ai/download_images", async (req, res) => {
   }
 });
 
-app.listen(port, () => console.log(`Server is running on port ${port}!!`));
+app.listen(port, () =>{
+  server.timeout = 300000;
+  console.log(`Server is running on port ${port}!!`)
+});
